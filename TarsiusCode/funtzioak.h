e190f8d61ff4ca6eaf7d1b097ca9baf2add7c77d
Imanol Rubio <imanol.rubio@alumni.mondragon.edu>
2018-05-22 12:25:32 +0200

    testIma

--- TarsiusCode/funtzioak.h
+++ TarsiusCode/funtzioak.h
@@ -1,15 +1,18 @@
 #ifndef FUNTZIOAK_H
 #define FUNTZIOAK_H
 #define MAX_KAR 120
-#define _CRT_SECURE_NO_WARNINGS
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-
+#include "SDL2/SDL.h"
+#include "SDL2/SDL_main.h"
 #include "SDL2/SDL_mixer.h"
+#include "SDL2/SDL_events.h"
+
+
 typedef struct soinuakGrabatu{
   int soinua;
   struct soinuakGrabatu *hurrengoSoinua;
 }SOINUAKGRABATU;
-
+void movement(void);
 #endif
--- TarsiusCode/main.c
+++ TarsiusCode/main.c
@@ -4,6 +4,72 @@
 
 int main (void){
 printf("Hello Tarsius Ander's change\n");
+movement();
 getchar();
-return 0;  
+return 0;
+}
+
+void movement(void) {
+	// process events
+	SDL_Event event;
+	while (SDL_PollEvent(&event))
+	{
+		switch (event.type)
+		{
+		case SDL_QUIT:
+			break;
+		case SDL_KEYDOWN:
+			switch (event.key.keysym.scancode)
+			{
+			case SDL_SCANCODE_1:
+				break;
+			case SDL_SCANCODE_2:
+				break;
+			case SDL_SCANCODE_W:
+				printf("w");
+			case SDL_SCANCODE_UP:
+				break;
+			case SDL_SCANCODE_A:
+			case SDL_SCANCODE_LEFT:
+				break;
+			case SDL_SCANCODE_S:
+			case SDL_SCANCODE_DOWN:
+				break;
+			case SDL_SCANCODE_D:
+			case SDL_SCANCODE_RIGHT:
+				break;
+			case SDL_SCANCODE_ESCAPE:
+				break;
+			}
+			default:
+				break;
+			break;
+		case SDL_KEYUP:
+			switch (event.key.keysym.scancode)
+			{
+			case SDL_SCANCODE_1:
+				break;
+			case SDL_SCANCODE_2:
+				break;
+			case SDL_SCANCODE_W:
+			case SDL_SCANCODE_UP:
+				break;
+			case SDL_SCANCODE_A:
+			case SDL_SCANCODE_LEFT:
+				break;
+			case SDL_SCANCODE_S:
+			case SDL_SCANCODE_DOWN:
+				break;
+			case SDL_SCANCODE_D:
+			case SDL_SCANCODE_RIGHT:
+				break;
+			case SDL_SCANCODE_ESCAPE:
+				break;
+			default:
+				break;
+			}
+			break;
+		}
+	}
+
 }
